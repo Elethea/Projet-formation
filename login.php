5701b774ff662c0e3e945ad34b7b89b21a7bc5dc
Lucas Auquier <auquierlucas@outlook.fr>
2018-11-05 12:08:42 +0100

    Captcha login

--- login.php
+++ login.php
@@ -1,12 +1,19 @@
 <?php
+require('php/recaptcha_valid.php');
+$client_IP = $_SERVER['REMOTE_ADDR'];
+
 SESSION_start();
-    if(isset($_POST['email'], $_POST['password'])){
+    if(isset($_POST['email'], $_POST['password'], $_POST['g-recaptcha-response'])){
         if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
             $errors[] = 'Email non valide';
         }
 
         if(!preg_match('/^.{5,500}$/', $_POST['password'])){
             $errors[]= 'Mot de passe non valide';
+        }
+
+        if(!recaptcha_valid($_POST['g-recaptcha-response'], $client_IP)){
+            $errors[] = 'Recaptcha invalide';
         }
 
         if(!isset($errors)){
@@ -27,14 +34,14 @@ SESSION_start();
 
             if(!empty($user)){
                 if(password_verify($_POST['password'], $user['password'])){
-                        $_SESSION['account']['email'] = $_POST['email'];
-                        $_SESSION['account']['name'] = $user['lastname'];
-                        $_SESSION['account']['firstname'] = $user['firstname'];
-                        $_SESSION['account']['date'] = $user['insc_date'];
+                    $_SESSION['account']['email'] = $_POST['email'];
+                    $_SESSION['account']['name'] = $user['lastname'];
+                    $_SESSION['account']['firstname'] = $user['firstname'];
+                    $_SESSION['account']['date'] = $user['insc_date'];
 
-                        $success = 'Vous êtes connecter';
-                    } else {
-                        $errors[] = 'Mot de passe invalide';
+                    $success = 'Vous êtes connecter';
+                } else {
+                    $errors[] = 'Mot de passe invalide';
                 }
             }else{
                 $errors[] = 'Il n\'existe pas de compte avec cet email';
@@ -50,6 +57,7 @@ SESSION_start();
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>se connecter</title>
+        <script src='https://www.google.com/recaptcha/api.js'></script>
     </head>
     <body>
     <?php
@@ -59,6 +67,7 @@ SESSION_start();
         <form action="login.php" method="post">
             <input type="email" placeholder="BobMoran@gmail.com" name="email" required>
             <input type="password" name="password"  required>
+            <div class="g-recaptcha" data-sitekey="6LeG8HcUAAAAAI9YCD0ZZnModHfaTm8o5irfoKYW"></div>
             <input type="submit" value="Connexion">
         </form>
     <?php
