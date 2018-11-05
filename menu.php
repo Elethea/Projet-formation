c0ec486697dc58847ea8d2a9e45f34b9e73783de
CuvelierCyril <cyrilcuvelier@sfr.fr>
2018-11-05 10:31:51 +0100

    Delete menu.php

--- menu.php
+++
@@ -1,9 +0,0 @@
-<nav>
-    <ul>
-        <li><a href="index.php">Acceuil</a></li>
-        <li><a href="inscription.php">Inscription</a></li>
-        <li><a href="login.php">Connexion</a></li>
-        <li><a href="logout.php">Déconnexion</a></li>
-        <li><a href="profil.php">Profil</a></li>
-    </ul>
-</nav>
