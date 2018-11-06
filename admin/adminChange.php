4af4693e99c962f090bdac65ad391f9ba41f2518
Lucas Auquier <auquierlucas@outlook.fr>
2018-11-06 14:30:42 +0100

    Corrections minimes + certains commentaires

--- admin/adminChange.php
+++ admin/adminChange.php
@@ -1,9 +1,13 @@
 <?php
 session_start();
+
+// On inclus notre fichier qui vérifie le niveau de droit de l'utilisateur dans la bdd
 require '../php/testadmin.php';
 
+// On verifie si le formulaire est bien remplis
 if(isset($_POST['title'], $_POST['content'], $_POST['id'])){
 
+    // On vérifie si le contenu du formulaire correspond à nos attentes
     if(!preg_match('#^.{2,250}$#',$_POST['title'])){
         $errors[] = 'Votre titre est invalide';
     }
@@ -69,11 +73,17 @@ if(isset($_POST['title'], $_POST['content'], $_POST['id'])){
 </head>
 <body>
     <h1>Modification Article</h1>
+
     <?php
+    // Inclusion du menu avec les liens relatifs partant du dossier admin
     include '../php/menu_admin.php';
 
+    // On vérifie si la personne est bien admin et si le token de connexion est fournis
     if($admin&& isset($_GET['token'])){
+
+        // On vérifie la véracité du cookie de connexion en le comparant à celui générer plus tôt
         if($_GET['token'] == $_SESSION['account']['token']){
+
             // Si success n'existe pas, on affiche le formulaire, sinon on affiche success
             if(!isset($success)){
 
@@ -96,6 +106,7 @@ if(isset($_POST['title'], $_POST['content'], $_POST['id'])){
                 }
             }
 
+        // Si le token ne correspond pas on coupe toutes fonctionnalités php et affiche une erreur
         }else {
             die('Jeton de session invalide');
         }
